b107a3994d3ab7ba9f2cc4892b007f231c1b678c
Nicholas <nicholasjoven@gmail.com>
2022-08-11 21:20:27 -0400

    commit tree fe19c085cddae9c29c21779cb062b9b7f17fb5a3

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."  # could be self.end_with?
   end
 
   def question?
-
+    self.end_with?("?") 
   end
 
   def exclamation?
-
+    self.end_with?("!") 
   end
 
   def count_sentences
-
+    self.split(/\.|\?|!/).filter { |sentence| !sentence.empty? }.length
   end
 end
